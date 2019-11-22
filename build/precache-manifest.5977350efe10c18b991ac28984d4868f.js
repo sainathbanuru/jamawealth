f2afb3d839b580855b75e15a27bde5b9f82bbc4e
Sainath Banuru <sainathbanuru@Sainaths-MBP.Dlink>
2019-11-23 00:19:22 +0530

    static site

---
+++ build/precache-manifest.5977350efe10c18b991ac28984d4868f.js
@@ -0,0 +1,334 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdd3b6b3dd230e47e1d31980f210f027",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1dce8aa0cf7e9680ef67",
+    "url": "/static/css/2.7d36abf2.chunk.css"
+  },
+  {
+    "revision": "0239bc768f62b2a15ce9",
+    "url": "/static/css/main.4c2b7569.chunk.css"
+  },
+  {
+    "revision": "1dce8aa0cf7e9680ef67",
+    "url": "/static/js/2.028694f9.chunk.js"
+  },
+  {
+    "revision": "0239bc768f62b2a15ce9",
+    "url": "/static/js/main.bf2d3f90.chunk.js"
+  },
+  {
+    "revision": "08fc46affaeed5a44c88",
+    "url": "/static/js/runtime-main.1eab234e.js"
+  },
+  {
+    "revision": "ef99020d8e5ef4d037f4a91f03e221bf",
+    "url": "/static/media/5trillion@2x.ef99020d.png"
+  },
+  {
+    "revision": "8ab436d2f964a15fceb99cf561e73f1d",
+    "url": "/static/media/Aboutusimg.8ab436d2.png"
+  },
+  {
+    "revision": "677b55a219b803dfa20a1b1a41ce4f65",
+    "url": "/static/media/Group 2284.677b55a2.svg"
+  },
+  {
+    "revision": "40b427ed057765f0f3f1538f74afd99a",
+    "url": "/static/media/Group10.40b427ed.svg"
+  },
+  {
+    "revision": "da71388ff6a18127c7a85db1efcba25f",
+    "url": "/static/media/Group11.da71388f.svg"
+  },
+  {
+    "revision": "ce5a738964672a2ab76a72cad774cd84",
+    "url": "/static/media/Group12.ce5a7389.svg"
+  },
+  {
+    "revision": "f56e501f24ee5c4ce294feb246257e44",
+    "url": "/static/media/Group13.f56e501f.svg"
+  },
+  {
+    "revision": "507930d0801f24e60ec7ef8bc6dfe722",
+    "url": "/static/media/Howjamaimg.507930d0.svg"
+  },
+  {
+    "revision": "3f76e1ebe3436f716b754b7f08ed44df",
+    "url": "/static/media/Jama_wealth_logo.3f76e1eb.svg"
+  },
+  {
+    "revision": "8545002625aa465f8fb8a8077a2bfaee",
+    "url": "/static/media/Manoj@2x.85450026.png"
+  },
+  {
+    "revision": "3d0565f2a4a846efb81eb024b93df672",
+    "url": "/static/media/Money Bag.3d0565f2.svg"
+  },
+  {
+    "revision": "bf5c339d2b204c8d2172229f78f24adf",
+    "url": "/static/media/Money-Bag-Market-Growth.bf5c339d.svg"
+  },
+  {
+    "revision": "8ee923964368d8481d0d1157fb0efc76",
+    "url": "/static/media/OurModelPortfolio.8ee92396.jpg"
+  },
+  {
+    "revision": "0a91fc7404f8841e9c6e1f7b44e49c90",
+    "url": "/static/media/Ram@2x.0a91fc74.png"
+  },
+  {
+    "revision": "c0ebf778571b41df949767adcc48e044",
+    "url": "/static/media/Rectangle1a.c0ebf778.png"
+  },
+  {
+    "revision": "d67cad150257444037ee750900d0428d",
+    "url": "/static/media/aboutimg.d67cad15.svg"
+  },
+  {
+    "revision": "0f00a4018d6f257338763a343ba79659",
+    "url": "/static/media/allocate.0f00a401.svg"
+  },
+  {
+    "revision": "38f9385c858042d9903e468a4f48df69",
+    "url": "/static/media/alocateimg.38f9385c.svg"
+  },
+  {
+    "revision": "c534646eeb573c494db9c6ca81caa3b1",
+    "url": "/static/media/asset_allocation@2x.c534646e.png"
+  },
+  {
+    "revision": "c3dd2ff36adffeddc3c7a084f87a31ed",
+    "url": "/static/media/bars.c3dd2ff3.svg"
+  },
+  {
+    "revision": "794103f0381324392f9907c0d781219b",
+    "url": "/static/media/barsLight.794103f0.svg"
+  },
+  {
+    "revision": "013222c14975d04f883ddf8ca9ad6d6b",
+    "url": "/static/media/bg_block_vector.013222c1.svg"
+  },
+  {
+    "revision": "01f59bb37bb7c6099b82f24494318614",
+    "url": "/static/media/compounding@2x.01f59bb3.png"
+  },
+  {
+    "revision": "27656e1c2c54fb349f50a85c61a39dba",
+    "url": "/static/media/diagram.27656e1c.svg"
+  },
+  {
+    "revision": "86d775855e9736484023be148b5d8700",
+    "url": "/static/media/ebook-bg.86d77585.jpg"
+  },
+  {
+    "revision": "801a6ea714f593da17796c87cc87b838",
+    "url": "/static/media/feature1.801a6ea7.JPG"
+  },
+  {
+    "revision": "835de43ef7a6c99d1f77484c40020de7",
+    "url": "/static/media/fivetrillioneconomy.835de43e.png"
+  },
+  {
+    "revision": "e880921b92b10665ccaad1deb804f056",
+    "url": "/static/media/homegraph.e880921b.svg"
+  },
+  {
+    "revision": "3d3cd85861ee122c74ba8e78e443d0a1",
+    "url": "/static/media/honesty.3d3cd858.svg"
+  },
+  {
+    "revision": "fda4070677a5a884bb6fcadbbeedb1d4",
+    "url": "/static/media/how-jama-wealth-img.fda40706.svg"
+  },
+  {
+    "revision": "f5da1c5524d455c824a5905a3fb26863",
+    "url": "/static/media/howweperformimg.f5da1c55.jpg"
+  },
+  {
+    "revision": "90b947282f4c12092ce0eb2c56e7aab2",
+    "url": "/static/media/investmentPhilosophyFocus.90b94728.png"
+  },
+  {
+    "revision": "c91cc78a58922d7243149dce0da0dadd",
+    "url": "/static/media/investmentPhilosophyFocus2.c91cc78a.png"
+  },
+  {
+    "revision": "f4886a39e30c51a545280552061c505d",
+    "url": "/static/media/investment_philosophy@2x.f4886a39.png"
+  },
+  {
+    "revision": "8598c83a9c246e5bdd50b2d035d36e26",
+    "url": "/static/media/jamaadvisory.8598c83a.png"
+  },
+  {
+    "revision": "a969a1cb532e38acb4640bf99e63f4cc",
+    "url": "/static/media/jargraph.a969a1cb.png"
+  },
+  {
+    "revision": "af48dd3a4aa9cb63e70c454e68ddbbdf",
+    "url": "/static/media/jarsBanner.af48dd3a.png"
+  },
+  {
+    "revision": "dc396b27cbf98cc335a08e0ccbda0e13",
+    "url": "/static/media/keystatsimg.dc396b27.jpg"
+  },
+  {
+    "revision": "60933ee7d3d6d19a2ac66c9d5a8f5ce8",
+    "url": "/static/media/left-arrow.60933ee7.svg"
+  },
+  {
+    "revision": "038f55138d6a1cf2a32034097b4d2595",
+    "url": "/static/media/linkedin-logo.038f5513.svg"
+  },
+  {
+    "revision": "645059c6debb913961d755a148dab217",
+    "url": "/static/media/linkedin_logo.645059c6.svg"
+  },
+  {
+    "revision": "2f69b0aecf4c1227521dedbf1accf1c8",
+    "url": "/static/media/magic-wand-light.2f69b0ae.svg"
+  },
+  {
+    "revision": "bb2dcbb025fadfb0d51367b3fa4db66c",
+    "url": "/static/media/magic-wand.bb2dcbb0.svg"
+  },
+  {
+    "revision": "7e0f9b6813387468dfba148642aee372",
+    "url": "/static/media/mobileBanner.7e0f9b68.png"
+  },
+  {
+    "revision": "841eac3ff6544aed1b49033c0afbc757",
+    "url": "/static/media/niftimg.841eac3f.png"
+  },
+  {
+    "revision": "723852f151a681ae7c87f2072f0da89d",
+    "url": "/static/media/nocusmoney.723852f1.svg"
+  },
+  {
+    "revision": "7a5f78e35c774c306c6abffc220c7e6f",
+    "url": "/static/media/nocustomshare.7a5f78e3.svg"
+  },
+  {
+    "revision": "8cea6b72a5c6fd2fcaac79b73b07bfe1",
+    "url": "/static/media/ourvaluebg.8cea6b72.svg"
+  },
+  {
+    "revision": "1cbecb8766cd0b7488245531c9bea893",
+    "url": "/static/media/ourvalueimg.1cbecb87.svg"
+  },
+  {
+    "revision": "2c932a071e705776dbcf70a7cb4bee6f",
+    "url": "/static/media/philosophyimg.2c932a07.jpg"
+  },
+  {
+    "revision": "d5bc6fab485ff320a100b92e8ac0f6b6",
+    "url": "/static/media/profile.d5bc6fab.PNG"
+  },
+  {
+    "revision": "81b9baf4561989ba204d7f9aaa67470f",
+    "url": "/static/media/profit.81b9baf4.svg"
+  },
+  {
+    "revision": "45f35cecb14d106607cf1d8d3276d531",
+    "url": "/static/media/promiselogoa.45f35cec.svg"
+  },
+  {
+    "revision": "8016bfffdedfbefd7d3f56ebda7e3c83",
+    "url": "/static/media/save.8016bfff.svg"
+  },
+  {
+    "revision": "b8198557d16d689e44d6d900ceb5cfd9",
+    "url": "/static/media/sheerimg.b8198557.svg"
+  },
+  {
+    "revision": "a94c9ae6a40ccfbe796048d7da32b7f2",
+    "url": "/static/media/speed.a94c9ae6.svg"
+  },
+  {
+    "revision": "538e9de30c0c4cf5a1797741ed48b331",
+    "url": "/static/media/startwealthcreation.538e9de3.svg"
+  },
+  {
+    "revision": "e327c20de96e69dd3f9e73139a956cfc",
+    "url": "/static/media/team1.e327c20d.JPG"
+  },
+  {
+    "revision": "180b45e8afaa33a1de32704463adc453",
+    "url": "/static/media/team2.180b45e8.JPG"
+  },
+  {
+    "revision": "60005b6bba2b31fb385f8da874701465",
+    "url": "/static/media/test.60005b6b.png"
+  },
+  {
+    "revision": "e2fb8c821e62f89997a0a048b9385926",
+    "url": "/static/media/testimonial-brcard.e2fb8c82.svg"
+  },
+  {
+    "revision": "ce7c7507c4618b4076657403bace7a8c",
+    "url": "/static/media/twitter_logo.ce7c7507.svg"
+  },
+  {
+    "revision": "bcdb09a0b8d50ab6b1f9877be9a15bc0",
+    "url": "/static/media/undraw_pie_chart_6efe.bcdb09a0.svg"
+  },
+  {
+    "revision": "ccc633e98c2e1552b02874131e0a1c70",
+    "url": "/static/media/undraw_transfer_money.ccc633e9.svg"
+  },
+  {
+    "revision": "3b944ad6d5f80892a65be9bb36cc9521",
+    "url": "/static/media/undraw_transfer_money_economy.3b944ad6.svg"
+  },
+  {
+    "revision": "706008c2b7d8e7208108bee6d9067d45",
+    "url": "/static/media/undraw_visual_data_b1wx.706008c2.svg"
+  },
+  {
+    "revision": "20c354e1162f6f8e55cd3f2b75162bda",
+    "url": "/static/media/undraw_visual_data_big.20c354e1.svg"
+  },
+  {
+    "revision": "706008c2b7d8e7208108bee6d9067d45",
+    "url": "/static/media/undraw_visual_data_white.706008c2.svg"
+  },
+  {
+    "revision": "4837f2cd45d9ea94aa870476cc6d0d3c",
+    "url": "/static/media/value.4837f2cd.svg"
+  },
+  {
+    "revision": "1370d46022a951d8ecdb6acc0f76c383",
+    "url": "/static/media/valueicon1.1370d460.svg"
+  },
+  {
+    "revision": "17482e066596fff9eddfe0e763bc0c3d",
+    "url": "/static/media/valueicon2.17482e06.svg"
+  },
+  {
+    "revision": "bc400255851b1d5d80efa9e39a8363a5",
+    "url": "/static/media/valueicon3.bc400255.svg"
+  },
+  {
+    "revision": "dc5329018f3db82a6b22482010662776",
+    "url": "/static/media/valueicon4.dc532901.svg"
+  },
+  {
+    "revision": "562678bc0208d7c3c80ad289f2cbc523",
+    "url": "/static/media/valueicon5.562678bc.svg"
+  },
+  {
+    "revision": "d12172db6342f954b5e7974579e45ef7",
+    "url": "/static/media/valueicon6.d12172db.svg"
+  },
+  {
+    "revision": "96c35a6ffbe401b45bfa9cd619dc8a49",
+    "url": "/static/media/videosectionbg.96c35a6f.svg"
+  },
+  {
+    "revision": "538e9de30c0c4cf5a1797741ed48b331",
+    "url": "/static/media/wealthcreaationimg.538e9de3.svg"
+  }
+]);
