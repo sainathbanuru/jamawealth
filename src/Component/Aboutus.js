65221e938166c4041c3fd343239632828a2e7683
Dinesh Reddy <dineshreddy@Dineshs-MacBook-Pro.local>
2019-12-18 16:22:25 +0530

    update

--- src/Component/Aboutus.js
+++ src/Component/Aboutus.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "../css/style.css";
+import Ram2x from "../assets/Ram@2x.png";
+import Manoj2x from "../assets/Manoj@2x.png";
 import "../css/about.css";
 import "../css/values.css";
 import aboutimg from "../assets/aboutimg.svg";
@@ -14,6 +16,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import WOW from "wowjs";
 import { Helmet } from "react-helmet";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 
 class Aboutus extends Component {
   componentDidMount() {
@@ -32,7 +35,6 @@ class Aboutus extends Component {
           ></meta>
         </Helmet>
         <Header />
-
         <div class="jars-section top-content-section value-top-section valign-wrapper">
           <div class="container w-percent-90 valign-wrapper">
             <div class="row valign-wrapper">
@@ -56,8 +58,7 @@ class Aboutus extends Component {
             </div>
           </div>
         </div>
-
-        <div class="jars-section">
+        {/* <div class="jars-section">
           <div class=" jars-section-title-heading ">
             <h4>
               <b>Founders</b>
@@ -100,8 +101,8 @@ class Aboutus extends Component {
                       <h6>Manoj Trivedi</h6>
                       <p>Research & Advisory Head</p>
                       {/* <li><img src={twitter} /></li> */}
-                      <li>
-                        <a
+        {/* <li> */}
+        {/* <a
                           href="https://www.linkedin.com/in/manoj-trivedi-a517b014/"
                           target="_blank"
                         >
@@ -113,9 +114,181 @@ class Aboutus extends Component {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
 
-          {/* <div class="container w-percent-90">
+        <div class="jars-section Wealth-curated-section">
+          <div class="container w-percent-90">
+            <div class="jars-section-title-heading">
+              <h4>Investment Advisors</h4>
+            </div>
+            <div class="row founders-section">
+              <div class="col s12 m6" style={{ width: "100%", height: "100%" }}>
+                <div
+                  class="Curator-card-section wow fadeIn"
+                  style={{ height: "100%" }}
+                >
+                  <div class="row valign-wrapper" style={{ marginBottom: 0 }}>
+                    <div class="col s3 no-padding">
+                      <LazyLoadImage
+                        class="curators-profile-img responsive-img"
+                        src={Manoj2x}
+                      />
+                    </div>
+                    <div class="col s9">
+                      <div class="curators-profile-title">
+                        <h5>Manoj Trivedi</h5>
+                        <h6 class="m-0">
+                          Research & Advisory Head{" "}
+                          <span>
+                            <a
+                              href="https://www.linkedin.com/in/manoj-trivedi-a517b014/"
+                              target="_blank"
+                            >
+                              <LazyLoadImage src={linkedin} />
+                            </a>
+                          </span>
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+                  <p class="SEBI-style">
+                    SEBI Registered Investment Advisor INA 200006999
+                  </p>
+
+                  <div class="row m-b-0">
+                    <div class="curator-description">
+                      <ul class="m-0">
+                        <li class="curator-description-li">
+                          Certified Equity Research Analyst
+                        </li>
+                        <li class="curator-description-li">
+                          Ex Consultant Global Top 10 IB & Wealth Management
+                          Firm
+                        </li>
+                        <li class="curator-description-li">
+                          Expert on Enterprise Valuation, Capital Structure &
+                          Project Finance
+                        </li>
+                        {/* <li class="curator-description-li">
+                          SEBI Registered Investment Advisor
+                        </li> */}
+                        <li class="curator-description-li">
+                          PGDM from IIM Bangalore. CA, CWA Ranker
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col s12 m6" style={{ width: "100%", height: "100%" }}>
+                <div
+                  class="Curator-card-section wow fadeIn"
+                  style={{ height: "100%" }}
+                >
+                  <div class="row valign-wrapper" style={{ marginBottom: 0 }}>
+                    <div class="col s3 no-padding">
+                      <LazyLoadImage
+                        class="curators-profile-img responsive-img"
+                        src={Ram2x}
+                      />
+                    </div>
+                    <div class="col s9">
+                      <div class="curators-profile-title">
+                        <h5>Ram Kalyan Medury</h5>
+                        <h6 class="m-0">
+                          Founder & CEO{" "}
+                          <span>
+                            <a
+                              href="https://www.linkedin.com/in/rammedury/"
+                              target="_blank"
+                            >
+                              <LazyLoadImage src={linkedin} />
+                            </a>
+                          </span>
+                        </h6>
+                      </div>
+                    </div>
+                  </div>
+
+                  <p class="SEBI-style">
+                    SEBI Registered Investment Advisor INA 200006725
+                  </p>
+
+                  <div class="row m-b-0">
+                    <div class="curator-description">
+                      <ul class="m-0">
+                        <li class="curator-description-li">
+                          Ex CXO at BFSI Majors, Fin Services Leader at Infosys
+                        </li>
+                        <li class="curator-description-li">
+                          Ex Consultant, Global Fortune 100 Firm in Financial
+                          Services & Investment Advisory
+                        </li>
+                        {/* <li class="curator-description-li">
+                          SEBI Registered Investment Advisor
+                        </li> */}
+                        <li class="curator-description-li">
+                          PGDM from IIM Bangalore
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="container how-jama-works-content w-percent-90">
+            <div
+              class="jars-section-title-heading"
+              style={{ marginTop: 64, marginBottom: 0 }}
+            >
+              <h4>As Featured In</h4>
+            </div>
+          </div>
+          <div className="featuredImageContainer">
+            <LazyLoadImage
+              src="https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/entrepreneur_india.png"
+              style={{ objectFit: "cover" }}
+              alt="Entrepreneur India"
+              className="featuredImages"
+            />
+            <LazyLoadImage
+              src={
+                "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/qrius.png"
+              }
+              style={{ objectFit: "cover" }}
+              alt="Qrius"
+              className="featuredImages"
+            />
+            <LazyLoadImage
+              src={
+                "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/money_control.png"
+              }
+              style={{ objectFit: "cover" }}
+              alt="Money Control"
+              className="featuredImages"
+            />
+            <LazyLoadImage
+              src={
+                "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/silicon_india.png"
+              }
+              style={{ objectFit: "cover" }}
+              alt="Silicon India"
+              className="featuredImages"
+            />
+            <LazyLoadImage
+              src={
+                "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/the_afternoon.png"
+              }
+              style={{ objectFit: "cover" }}
+              alt="The Afternoon"
+              className="featuredImages"
+            />
+          </div>
+        </div>
+        {/* <div class="container w-percent-90">
             <div class=" jars-section-title-heading ">
               <h4>
                 <b>Team</b>
@@ -221,8 +394,7 @@ class Aboutus extends Component {
               </div> 
             </div>
           </div> */}
-        </div>
-
+        {/* </div> */}
         <div class=" JAR-contant-button center-align ">
           <div class=" row ">
             <div class=" col m12 ">
@@ -240,7 +412,6 @@ class Aboutus extends Component {
             </div>
           </div>
         </div>
-
         <Footer />
       </div>
     );
