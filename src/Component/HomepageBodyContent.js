9003c421983eaeadc57efbd82954edc165fe6f11
Sainath Banuru <sainathbanuru@Sainaths-MacBook-Pro.local>
2019-12-26 17:30:04 +0530

    added iframes

--- src/Component/HomepageBodyContent.js
+++ src/Component/HomepageBodyContent.js
@@ -465,10 +465,73 @@ class HomepageBodyContent extends Component {
                 </div>
               </div>
             </div>
+
+            <div class="row m-0">
+              <div class="col m6 l4">
+                <div
+                  class="jama-testimonials-cards wow fadeIn"
+                  data-wow-delay="0s"
+                  style={{ padding: 0, backgroundColor: "transparent" }}
+                >
+                  <iframe
+                    src="https://player.vimeo.com/video/378717516"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                    style={{
+                      position: "absolute",
+                      height: "100%",
+                      width: "100%"
+                    }}
+                  ></iframe>
+                </div>
+              </div>
+
+              <div class="col m6 l4">
+                <div
+                  class="jama-testimonials-cards wow fadeIn"
+                  data-wow-delay="0.2s"
+                  style={{ padding: 0, backgroundColor: "transparent" }}
+                >
+                  <iframe
+                    src="https://player.vimeo.com/video/378717516"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                    style={{
+                      position: "absolute",
+                      height: "100%",
+                      width: "100%"
+                    }}
+                  ></iframe>
+                </div>
+              </div>
+
+              <div class="col m6 l4">
+                <div
+                  class="jama-testimonials-cards wow fadeIn"
+                  data-wow-delay="0.4s"
+                  style={{ padding: 0, backgroundColor: "transparent" }}
+                >
+                  <iframe
+                    src="https://player.vimeo.com/video/378717516"
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen
+                    style={{
+                      position: "absolute",
+                      height: "100%",
+                      width: "100%"
+                    }}
+                  ></iframe>
+                </div>
+              </div>
+            </div>
             <div class="col m5 l4" style={{ padding: "0.75rem" }}>
               <div
                 class="jama-testimonials-vcards wow fadeIn"
                 data-wow-delay="0.4s"
+                style={{ padding: 0 }}
               >
                 <iframe
                   src="https://player.vimeo.com/video/378717516"
@@ -492,6 +555,44 @@ class HomepageBodyContent extends Component {
                 </button>{" "}
               </Link>
             </div> */}
+          </div>
+        </div>
+        <div style={{ display: "grid", gridTemplateColumns: "1fr 1fr 1fr" }}>
+          <div
+            class="jama-testimonials-vcards wow fadeIn"
+            data-wow-delay="0.4s"
+          >
+            <iframe
+              src="https://player.vimeo.com/video/378717516"
+              frameborder="0"
+              allow="autoplay; fullscreen"
+              allowfullscreen
+              style={{ position: "absolute", height: "70%", width: "100%" }}
+            ></iframe>
+          </div>
+          <div
+            class="jama-testimonials-vcards wow fadeIn"
+            data-wow-delay="0.4s"
+          >
+            <iframe
+              src="https://player.vimeo.com/video/378717516"
+              frameborder="0"
+              allow="autoplay; fullscreen"
+              allowfullscreen
+              style={{ position: "absolute", height: "70%", width: "100%" }}
+            ></iframe>
+          </div>
+          <div
+            class="jama-testimonials-vcards wow fadeIn"
+            data-wow-delay="0.4s"
+          >
+            <iframe
+              src="https://player.vimeo.com/video/378717516"
+              frameborder="0"
+              allow="autoplay; fullscreen"
+              allowfullscreen
+              style={{ position: "absolute", height: "70%", width: "100%" }}
+            ></iframe>
           </div>
         </div>
 
