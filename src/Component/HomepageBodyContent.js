34b06d67e6f655a24df3e117409f5a308dfbf2d6
Dinesh Reddy <dineshreddy@Dineshs-MacBook-Pro.local>
2020-01-02 14:51:11 +0530

    testimonial

--- src/Component/HomepageBodyContent.js
+++ src/Component/HomepageBodyContent.js
@@ -368,7 +368,68 @@ class HomepageBodyContent extends Component {
               <h4 class="testimonials-title-style">Testimonials</h4>
             </div>
             <div class="row m-0">
-              <div class="col m6 l4">
+              <div class="row m-0">
+                <div class="col m6 l4">
+                  <div
+                    class="jama-testimonials-cards wow fadeIn"
+                    data-wow-delay="0s"
+                    style={{ padding: 0, backgroundColor: "transparent" }}
+                  >
+                    <iframe
+                      src="https://player.vimeo.com/video/380209697"
+                      frameborder="0"
+                      allow="autoplay; fullscreen"
+                      allowfullscreen
+                      style={{
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%"
+                      }}
+                    ></iframe>
+                  </div>
+                </div>
+
+                <div class="col m6 l4">
+                  <div
+                    class="jama-testimonials-cards wow fadeIn"
+                    data-wow-delay="0.2s"
+                    style={{ padding: 0, backgroundColor: "transparent" }}
+                  >
+                    <iframe
+                      src="https://player.vimeo.com/video/380443962"
+                      frameborder="0"
+                      allow="autoplay; fullscreen"
+                      allowfullscreen
+                      style={{
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%"
+                      }}
+                    ></iframe>
+                  </div>
+                </div>
+
+                <div class="col m6 l4">
+                  <div
+                    class="jama-testimonials-cards wow fadeIn"
+                    data-wow-delay="0.4s"
+                    style={{ padding: 0, backgroundColor: "transparent" }}
+                  >
+                    <iframe
+                      src="https://player.vimeo.com/video/381501489"
+                      frameborder="0"
+                      allow="autoplay; fullscreen"
+                      allowfullscreen
+                      style={{
+                        position: "absolute",
+                        height: "100%",
+                        width: "100%"
+                      }}
+                    ></iframe>
+                  </div>
+                </div>
+              </div>
+              {/* <div class="col m6 l4">
                 <div
                   class="jama-testimonials-cards wow fadeIn"
                   data-wow-delay="0s"
@@ -391,7 +452,7 @@ class HomepageBodyContent extends Component {
                       </div>
                     </div>
                     <div class="testimonial-description">
-                      {/* <h6>The best of the best..</h6> */}
+                      {/* <h6>The best of the best..</h6> 
                       <p>
                         Jama financial advisors have made Financial Planning
                         truly comprehensive and unbiased.
@@ -399,134 +460,85 @@ class HomepageBodyContent extends Component {
                     </div>
                   </div>
                 </div>
-              </div>
+              </div> */}
 
-              <div class="col m6 l4">
-                <div
-                  class="jama-testimonials-cards wow fadeIn"
-                  data-wow-delay="0.2s"
-                >
-                  <div class="Testimonial-profile">
-                    <div class="row valign-wrapper testimonial-section-profile-details">
-                      <div class="testimonial-pic-section">
-                        <LazyLoadImage
-                          class="profile-pic"
-                          src={
-                            "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/new/static/img/Image+6.png"
-                          }
-                        />
-                      </div>
+              <div
+                class="row"
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center"
+                }}
+              >
+                <div style={{ display: "flex" }}>
+                  <div class="col m6 l4">
+                    <div
+                      class="jama-testimonials-cards wow fadeIn"
+                      data-wow-delay="0.2s"
+                    >
+                      <div class="Testimonial-profile">
+                        <div class="row valign-wrapper testimonial-section-profile-details">
+                          <div class="testimonial-pic-section">
+                            <LazyLoadImage
+                              class="profile-pic"
+                              src={
+                                "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/new/static/img/Image+6.png"
+                              }
+                            />
+                          </div>
 
-                      <div class="testimonial-text">
-                        <h6>Abhijit Sen</h6>
-                        <p class="m-0">Businessman</p>
+                          <div class="testimonial-text">
+                            <h6>Abhijit Sen</h6>
+                            <p class="m-0">Businessman</p>
+                          </div>
+                        </div>
+                        <div class="testimonial-description">
+                          {/* <h6>The best of the best..</h6> */}
+                          <p>
+                            Thanks to Jama Wealth Advisory, I could start my
+                            business with confidence. Money works hard for me
+                            now.
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                    <div class="testimonial-description">
-                      {/* <h6>The best of the best..</h6> */}
-                      <p>
-                        Thanks to Jama Wealth Advisory, I could start my
-                        business with confidence. Money works hard for me now.
-                      </p>
                     </div>
                   </div>
-                </div>
-              </div>
 
-              <div class="col m6 l4">
-                <div
-                  class="jama-testimonials-cards wow fadeIn"
-                  data-wow-delay="0.4s"
-                >
-                  <div class="Testimonial-profile">
-                    <div class="row valign-wrapper testimonial-section-profile-details">
-                      <div class="testimonial-pic-section">
-                        <LazyLoadImage
-                          class="profile-pic"
-                          src={
-                            "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/new/static/img/Image+7.png"
-                          }
-                        />
-                      </div>
+                  <div class="col m6 l4" style={{ marginRight: "auto" }}>
+                    <div
+                      class="jama-testimonials-cards wow fadeIn"
+                      data-wow-delay="0.4s"
+                    >
+                      <div class="Testimonial-profile">
+                        <div class="row valign-wrapper testimonial-section-profile-details">
+                          <div class="testimonial-pic-section">
+                            <LazyLoadImage
+                              class="profile-pic"
+                              src={
+                                "https://jamaappprod.s3-ap-southeast-1.amazonaws.com/Website/new/static/img/Image+7.png"
+                              }
+                            />
+                          </div>
 
-                      <div class="testimonial-text">
-                        <h6>Gautam Khanna</h6>
-                        <p class="m-0">Vice President, IT MNC</p>
+                          <div class="testimonial-text">
+                            <h6>Gautam Khanna</h6>
+                            <p class="m-0">Vice President, IT MNC</p>
+                          </div>
+                        </div>
+                        <div class="testimonial-description">
+                          {/* <h6>The best of the best..</h6> */}
+                          <p>
+                            Jama Wealth’s zero commission, zero brokerage
+                            advisory model builds trust. It is refreshing.
+                          </p>
+                        </div>
                       </div>
-                    </div>
-                    <div class="testimonial-description">
-                      {/* <h6>The best of the best..</h6> */}
-                      <p>
-                        Jama Wealth’s zero commission, zero brokerage advisory
-                        model builds trust. It is refreshing.
-                      </p>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="row m-0">
-              <div class="col m6 l4">
-                <div
-                  class="jama-testimonials-cards wow fadeIn"
-                  data-wow-delay="0s"
-                  style={{ padding: 0, backgroundColor: "transparent" }}
-                >
-                  <iframe
-                    src="https://player.vimeo.com/video/380209697"
-                    frameborder="0"
-                    allow="autoplay; fullscreen"
-                    allowfullscreen
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%"
-                    }}
-                  ></iframe>
-                </div>
-              </div>
-
-              <div class="col m6 l4">
-                <div
-                  class="jama-testimonials-cards wow fadeIn"
-                  data-wow-delay="0.2s"
-                  style={{ padding: 0, backgroundColor: "transparent" }}
-                >
-                  <iframe
-                    src="https://player.vimeo.com/video/380443962"
-                    frameborder="0"
-                    allow="autoplay; fullscreen"
-                    allowfullscreen
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%"
-                    }}
-                  ></iframe>
-                </div>
-              </div>
-
-              <div class="col m6 l4">
-                <div
-                  class="jama-testimonials-cards wow fadeIn"
-                  data-wow-delay="0.4s"
-                  style={{ padding: 0, backgroundColor: "transparent" }}
-                >
-                  <iframe
-                    src="https://player.vimeo.com/video/381501489"
-                    frameborder="0"
-                    allow="autoplay; fullscreen"
-                    allowfullscreen
-                    style={{
-                      position: "absolute",
-                      height: "100%",
-                      width: "100%"
-                    }}
-                  ></iframe>
-                </div>
-              </div>
-            </div>
             {/* <div class="col m5 l4" style={{ padding: "0.75rem" }}>
               <div
                 class="jama-testimonials-vcards wow fadeIn"
